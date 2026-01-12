7a4102349d9ea869b69c97ce6465e93cda491a39
azharmd-dev <azharmd0794@gmail.com>
2026-01-12 16:36:19 +0530

    VPC

---
+++ 00-vpc/parameters.tf
@@ -0,0 +1,23 @@
+resource "aws_ssm_parameter" "vpc_id" {
+  name  = "/${var.project_name}/${var.environment}/vpc_id"
+  type  = "String"
+  value = module.vpc.vpc_id
+}
+
+resource "aws_ssm_parameter" "public_subnet" {
+  name  = "/${var.project_name}/${var.environment}/public_subnet_id"
+  type  = "StringList"
+  value = join("," , module.vpc.public_subnet)
+}
+
+resource "aws_ssm_parameter" "private_subnet" {
+  name  = "/${var.project_name}/${var.environment}/private_subnet_id"
+  type  = "StringList"
+  value = join("," , module.vpc.private_subnet)
+}
+
+resource "aws_ssm_parameter" "database_subnet" {
+  name  = "/${var.project_name}/${var.environment}/database_subnet_id"
+  type  = "StringList"
+  value = join("," , module.vpc.database_subnet)
+}
