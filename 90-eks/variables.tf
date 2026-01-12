b93dcd0bf4181d33eaf5eef498705aa1135e7c9d
azharmd-dev <azharmd0794@gmail.com>
2026-01-12 18:56:11 +0530

    eks cluster setup using terraform

---
+++ 90-eks/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  default = "robomart"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0553382WPE1E3VPCYS6"
+}
+
+variable "domain_name" {
+    default = "azharprojects.site"
+}
