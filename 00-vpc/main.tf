fef276d9040467ac04c5c969543b377f2b17d2d5
azharmd-dev <azharmd0794@gmail.com>
2026-01-12 17:38:04 +0530

    VPC setup eks

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,15 +1,10 @@
 module "vpc" {
   source = "git::https://github.com/azharmd-dev/tf-aws-vpc.git?ref=main"
-#   vpc_cidr = "10.0.0.0/16"
-#   project_name = "robomart"
-#   environment = "dev"
-
   vpc_cidr = var.vpc_cidr
   project_name = var.project_name
   environment = var.environment
   public_subnet_cidr = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
   database_subnet_cidr = var.database_subnet_cidr
-
   is_peering_required = true
 }
