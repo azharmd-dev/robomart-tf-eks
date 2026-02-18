125f2bc710e199c136792b811921509ccde73cf6
azharmd-dev <azharmd0794@gmail.com>
2026-02-18 14:53:08 +0530

    eks cluster

--- 90-eks/variables.tf
+++ 90-eks/variables.tf
@@ -22,9 +22,9 @@ variable "eks_nodegroup_blue_version" {
 
 }
 
-variable "eks_nodegroup_green_version" {
+# variable "eks_nodegroup_green_version" {
 
-}
+# }
 
 variable enable_blue {
   type = bool
